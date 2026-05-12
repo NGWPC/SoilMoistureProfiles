5cf64fb069b00d13cf82389221b0496d85d0a896
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-05-12 08:30:14 -0700

    Remove using generic EWTS_ID. Define LOG in submodule instead of relying on generic definition in ewts library.

--- include/Logger.hxx
+++ include/Logger.hxx
@@ -2,18 +2,16 @@
 #define SMP_LOGGER_HPP
 
 #include "ewts/module_constants.hpp"
-
-// Provide the constant in the global namespace
-inline constexpr const char* EWTS_ID_SMP = ewts::modules::EWTS_ID_SMP;
-
-// Bind this module's logger identity
-#define EWTS_ID EWTS_ID_SMP
-
 #include "ewts/logger.hpp"
+#include "ewts/log_levels.hpp"
+
+#define LOG(...) ::ewts::GetLogger(::ewts::modules::EWTS_ID_SMP).Log(__VA_ARGS__)
+#define GetLogLevel() ::ewts::GetLogger(::ewts::modules::EWTS_ID_SMP).GetLogLevel()
+#define IsLoggingEnabled() ::ewts::GetLogger(::ewts::modules::EWTS_ID_SMP).IsLoggingEnabled()
 
 using ewts::EwtsInit;
 using ewts::LogLevel;
-using ewts::GetLogLevel;
 
+inline constexpr const char* SMP_MODULE_ID = ewts::modules::EWTS_ID_SMP;
 
 #endif /* SMP_LOGGER_HPP */
